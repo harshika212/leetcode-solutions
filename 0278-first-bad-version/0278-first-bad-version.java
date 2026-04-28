6c41952773eee056eff6769aa78fafe01063f70d
harshika212 <harshikasingh212@gmail.com>
2026-04-28 22:07:43 +0530

    Time: 14 ms (49.23%), Space: 42.3 MB (18.11%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,17 +3,20 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start=1;
-        int end=n;
-        while(start<end){
-            int mid = start + (end - start)/2;
-            if(isBadVersion(mid)){
-                end = mid;
-            }else{
-                start = mid + 1;
-            }
-            
+        int start = 1, end = n;
+    int ans = n;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (isBadVersion(mid)) {
+            ans = mid;        
+            end = mid - 1;    
+        } else {
+            start = mid + 1;  
         }
-        return start;
+    }
+
+    return ans; 
     }
 }
